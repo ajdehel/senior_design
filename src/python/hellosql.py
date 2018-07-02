8929aeb48ba5addaeafa9c9ea474782860ee10fc
andrew dehel <dehel.andrew@gmail.com>
2018-07-02 09:58:30 -0400

    Move contents of hellosql.py into test_sql.py
    hellosql.py now contains a helloworld type demonstration of sql.py like
    hellomqtt.py for mqtt.py.

--- src/python/hellosql.py
+++ src/python/hellosql.py
@@ -1,33 +1,9 @@
 import sql
-import unittest
-from unittest import mock
 
-class TestDatabase(unittest.TestCase):
+server = "mssql-instance.cfblwhszdeig.us-east-2.rds.amazonaws.com"
+database = "mydb"
+username = "mssql"
+password = "mssql"
 
-    def setUp(self):
-        self.mocked_Connection = mock.Mock()
-        self.mocked_Cursor = mock.Mock()
-        self.patched_Connect = mock.patch("pypyodbc.connect")
-        self.patched_Connect.start()
-        self.mocked_Connect = self.patched_Connect.start()
-        self.mocked_Connect.return_value = self.mocked_Connection
-        self.mocked_Connection.cursor.return_value = self.mocked_Cursor
+db = sql.Database('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
-    def test__insert_into__with_columns(self):
-        db = sql.Database()
-        values = (1, 14.0, 0)
-        columns = ("deviceID", "metricA", "metricB")
-        db.insert_into("HelloTable", values, columns=columns)
-        print(self.mocked_Cursor.execute.call_args)
-
-    def test__insert_into__without_columns(self):
-        db = sql.Database()
-        values = (1, 14.0, 0)
-        db.insert_into("HelloTable", values)
-        print(self.mocked_Cursor.execute.call_args)
-
-    def tearDown(self):
-        self.patched_Connect.stop()
-
-if __name__ == "__main__":
-    unittest.main()
