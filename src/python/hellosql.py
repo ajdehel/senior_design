ec36ee7b45c9d45823d55a160b2f21aee3b734c5
andrew dehel <dehel.andrew@gmail.com>
2018-07-03 19:54:47 +0000

    Remove "hello" files
    The demo files are no longer needed.
        deleted:    hellomqtt.py
        deleted:    hellosql.py

--- src/python/hellosql.py
+++
@@ -1,9 +0,0 @@
-import sql
-
-server = "mssql-instance.cfblwhszdeig.us-east-2.rds.amazonaws.com"
-database = "mydb"
-username = "mssql"
-password = "mssql"
-
-db = sql.Database('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-
