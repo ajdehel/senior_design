c2be0064897b42ebd8e6ac0bc47d59d52f7ed184
andrew dehel <dehel.andrew@gmail.com>
2018-07-03 18:35:21 +0000

    Add db connection to Client and catch JSON errors

--- src/python/mqtt.py
+++ src/python/mqtt.py
@@ -1,15 +1,10 @@
+import datetime
 import json
 import logging
 import paho.mqtt.client as mqtt
 import time
 
-LOGGER = logging.getLogger(__name__)
-LOG_FORMAT = "{asctime} -- {levelname} -- {filename} -- {message}"
-FORMATTER = logging.Formatter(fmt=LOG_FORMAT, style="{")
-CONSOLE_HANDLER = logging.StreamHandler()
-CONSOLE_HANDLER.setFormatter(FORMATTER)
-LOGGER.addHandler(CONSOLE_HANDLER)
-LOGGER.setLevel(logging.DEBUG)
+LOGGER = logging.getLogger("PLUMBINTEL")
 
 class Client(mqtt.Client):
 
@@ -17,6 +12,7 @@ class Client(mqtt.Client):
         super().__init__(**kwargs)
         self.ip_addr = ip_addr
         self.port = port
+        self.database = None
         self.on_connect = on_connect
         self.on_subscribe = on_subscribe
         self.on_message = on_message
@@ -30,6 +26,11 @@ class Client(mqtt.Client):
         else:
             super().connect(self.ip_addr, self.port, **kwargs)
 
+    def accept_database(self, database, table, ):
+        if database.connected:
+            self.database = database
+            self.db_target_table = table
+
     def __enter__(self):
         self.connect()
         return self
@@ -42,6 +43,13 @@ class Client(mqtt.Client):
     def check_for_data(self):
         self.loop(timeout=5.0)
 
+    def handle_message(self, payload):
+        if self.database:
+            if len(payload) is 3:
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                payload = payload[0:2] + [timestamp,] + payload[2:]
+            self.database.update(self.db_target_table, payload)
+
 def on_connect(client, userdata, flags, rc):
     LOGGER.info(f"client connected to {client.ip_addr}")
 
@@ -50,6 +58,10 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 def on_message(client, userdata, message):
     LOGGER.info("message received")
-    payload = json.loads(message.payload, encoding="utf-8")
-    LOGGER.debug(f"payload: {payload}")
+    try:
+        payload = json.loads(message.payload, encoding="utf-8")
+        LOGGER.debug(f"payload: {payload}")
+        client.handle_message(payload)
+    except json.decoder.JSONDecodeError as error:
+        LOGGER.error(f"received improperly formatted payload {str(message.payload)}")
 
