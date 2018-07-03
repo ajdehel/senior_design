09ed0dcde8152fb107b41d32f5090e57ad170225
andrew dehel <dehel.andrew@gmail.com>
2018-07-03 18:34:19 +0000

    Add working version of sql.py
    Only exploratory testing thus far has been done.

--- src/python/sql.py
+++ src/python/sql.py
@@ -1,32 +1,56 @@
-import pypyodbc
 import logging
+import pypyodbc
 
-CONNECTION_STRING = ''
+LOGGER = logging.getLogger("PLUMBINTEL")
 
 class Database:
 
-    def __init__(self, connection_string=CONNECTION_STRING):
-        self.connection = pypyodbc.connect(connection_string)
-        self.cursor = self.connection.cursor()
+    def __init__(self, driver=None, server=None, port=None,
+                 database=None, username=None, password=None, **kwargs):
+        connection_string = (f"DRIVER={{{driver}}};SERVER={server};PORT={port};"
+                             f"DATABASE={database};UID={username};PWD={password}")
+        LOGGER.debug(connection_string)
+        self.connection = pypyodbc.connect(connection_string, **kwargs)
+        LOGGER.info("Connection Succeeded.")
 
     def __enter__(self):
         return self
 
     def __exit__(self, e_type, e_value, e_traceback):
-        self.cursor.close()
+        if e_type:
+            LOGGER.error(str(e_type))
         self.connection.close()
 
     def validate(self, json):
         """Ensure new data is valid for the database."""
         pass
 
-    def insert_into(self, table_name, values, columns=""):
-        """Method to insert data into a given table."""
+    def update(self, table_name, values, columns=""):
+        """Method to update data in a given table.
+            will also insert new data into the table if the first
+            column item in values is not found in the database.
+        """
+        self._insert(table_name, values, columns)
+
+    def _insert(self, table_name, values, columns=""):
+        """Underlying method to insert new values into a given data."""
         col_string = ""
         if columns != "":
             columns = ", ".join(columns)
             col_string = f"({columns})"
-        values = ", ".join([str(value) for value in values])
+        values = ", ".join([ "'"+str(value)+"'" if type(value) is str else str(value) for value in values ])
         val_string = f"({values})"
-        self.cursor.execute(f"INSERT INTO {table_name} {col_string} VALUES {val_string}")
+        insert_string = f"INSERT INTO {table_name} {col_string} VALUES {val_string};"
+        LOGGER.debug(insert_string)
+        try:
+            with self.connection.cursor().execute(insert_string):
+                LOGGER.info("Insert Succeeded.")
+        except pypyodbc.DataError as error:
+            LOGGER.error(error)
 
+    def _update(self, table_name, values, clause, columns):
+        """Underlying method to update an existing entry in a given table."""
+
+    @property
+    def connected(self):
+        return self.connection.connected
