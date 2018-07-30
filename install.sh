55d96af68babb283347c503d02863af35b8ea6ce
andrew dehel <dehel.andrew@gmail.com>
2018-07-30 17:58:08 +0000

    Complete "install.sh" script for installation

--- install.sh
+++ install.sh
@@ -14,6 +14,27 @@ apt-get install mosquitto-clients
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get install python3.6
+### Install pip for Python3
+apt-get install python3-pip
 ### Setup python 3.6
-
-
+ln -s /usr/local/lib/python3.5 /usr/local/lib/python3.6
+if [ -e /usr/bin/python3 ]; then
+    echo "Changing symbolic link for /usr/bin/python3"
+    rm /usr/bin/python3
+fi
+ln -s /usr/bin/python3 /usr/bin/python3.6
+### MSSQL installs
+apt-get install msodbcsql
+apt-get install mssql-tools
+apt-get install unixodbc-dev
+apt-get install mssql-server
+apt-get update
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2014.list | sudo tee /etc/apt/sources.list.d/mssql-server-2014.list
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list | sudo tee /etc/apt/sources.list.d/mssql-server-2017.list
+/opt/mssql/bin/mssql-conf setup
+### Install Plumbintel package
+cd src
+pip install .
+exit
