45210f339ec703cd0ee61c5dcd6ecbf737cf4365
andrew dehel <dehel.andrew@gmail.com>
2018-08-02 16:17:01 +0000

    Increase DB timeout to 10 minutes

--- src/plumbintel/__main__.py
+++ src/plumbintel/__main__.py
@@ -10,7 +10,7 @@ from . import CONF_PATH, configure, insert_into_database
 
 LOGGER = logging.getLogger("PLUMBINTEL")
 def socket_interface(config):
-    with sql.Database(**config["SQL"]["Connection"], timeout=30) as db:
+    with sql.Database(**config["SQL"]["Connection"], timeout=600) as db:
         with udp.Listener(ip_addr=config["Listener"]["host"],
                           port=config["Listener"]["port"]) as listener:
             listener.start_thread()
@@ -20,7 +20,7 @@ def socket_interface(config):
                     insert_into_database(db, config["SQL"]["table"], listener.message_queue)
 
 def mqtt_interface(config):
-    with sql.Database(**config["SQL"]["Connection"], timeout=30) as db:
+    with sql.Database(**config["SQL"]["Connection"], timeout=600) as db:
         with mqtt.Client(ip_addr=config["MQTT"]["host"],
                           port=config["MQTT"]["port"]) as client:
             client.subscribe(config["MQTT"]["topic"])
